99ca32905a7a2c4f4b3a0bbea612727a67a73aee
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-08-05 16:26:50 +0200

    tested TW @apply directive in App.css background

--- ama-app/src/Button.js
+++ ama-app/src/Button.js
@@ -10,7 +10,7 @@
 // FUNCTIONAL COMPONENT (HOOK?)
 function Button() {
     return (
-        <button className="btn " type="button">Super CTA</button>
+        <button className="btn font-semibold hover:font-light hover:bg-green-800" type="button">Super CTA</button>
     );
 }
 export default Button;
--- ama-app/tailwind.config.js
+++ ama-app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./ama-app/src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
