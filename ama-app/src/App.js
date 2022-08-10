89dd2888c4b7591b081d45b6d375969f97ad5b87
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-08-10 23:33:23 +0200

    bind search input value to amazon filter method

--- ama-app/src/App.js
+++ ama-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
 import Rolodex from './components/Rolodex'
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
         <Welcome name='Amazonia' /> 
 
--- ama-app/src/components/Rolodex.js
+++ ama-app/src/components/Rolodex.js
@@ -1,75 +1,122 @@
 import React, { Component } from 'react';
 import Button from '../Button';
 
+// const { render, search } = useFilters();
 class Rolodex extends Component {
     constructor(props) {
+        console.log('constructor');
         super(props);
 
-        this.changeName = this.changeName.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        // this.changeName = this.changeName.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleSearchInput = this.handleSearchInput.bind(this);
 
         this.state = {
-            name: { firstName: 'Vincent',
-            lastName: 'Delaleu' },
+            name: { firstName: 'Vincent', lastName: 'Delaleu' },
             company: 'Argil Data Agency',
-            amazons:  [
-                {
-                    name: 'Golden Lion Tamarin',
-                },
-                {
-                    name: 'Jaguar ',
-                },
-                {
-                    name: 'Amazon River Dolphin',
-                },
-                {
-                    name: 'Giant Otters',
-                },
-                {
-                    name: 'Uakari Monkey',
-                },
-            ]
+            amazons:  [ ],
+            searchInputValue: '',
+            // amazons:  [
+            //     {
+            //         name: 'Golden Lion Tamarin',
+            //     },
+            //     {
+            //         name: 'Jaguar ',
+            //     },
+            //     {
+            //         name: 'Amazon River Dolphin',
+            //     },
+            //     {
+            //         name: 'Giant Otters',
+            //     },
+            //     {
+            //         name: 'Uakari Monkey',
+            //     },
+            // ]
         };
     }
 
-    handleChange(e) {
-        e.preventDefault();
-        console.log('Handle state');
+    componentDidMount() {
+        console.log('componentDidMount');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState(() => {
+                return {amazons: users}
+        },
+        () => {
+            // console.log(this.state);
+            // console.log(this.state.searchInputValue);
+        }
+        ));
+    }
+
+    // const [ name, setText ] = useState();
+
+    handleSearchInput(event) {
+        
+        console.log(event.target.value);
+        // console.log(this.state.searchInputValue);
+        // let searchInputValue = event.target.value;
+        this.setState({
+            searchInputValue: event.target.value 
+        })
+        // return (searchInputValue);
+        
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        console.log('Handle state: ' + event );
         this.setState(() => {
             return {
                 name: { firstName: 'ADA', lastName: 'DATA' },
             };
         });
-        console.log(e);
-
+        // console.log(e);
     }
 
-    changeName(e) {
-        e.preventDefault();
-        console.log('Change state');
-        this.setState(() => {
-            return {
-                name: { firstName: 'David', lastName: 'Morard' },
-            };
-        });
-    }
+    // changeName(e) {
+    //     e.preventDefault();
+    //     console.log('Change state');
+    //     this.setState(() => {
+    //         return {
+    //             name: { firstName: 'David', lastName: 'Morard' },
+    //         };
+    //     });
+    // }
 
     render() {
+        // let amazons;
+        // let searchInputValue;
+
+        console.log('render');
+         
         return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col items-center justify-center w-max'>
         <p className='text-xl font-semibold' >
             Hi {this.state.name.firstName} {this.state.name.lastName}, I work with {this.state.company}.
         </p>
-        Amazons Rolodex
-        
-        <ul>
 
-        {
-            this.state.amazons.map((amazon) => {
-                return <li className='font-semibold text-green-900'>{amazon.name}</li>;       
-            })
+        <h1>Amazons Rolodex</h1>
+
+        <input 
+            className='search-box text-green-900 my-8' 
+            type='search' 
+            placeholder='search amazons' 
+            onChange={ this.handleSearchInput }
+        />
+
+        <div className='grid grid-cols-3'>
+        {/* { this.state.amazons.map((amazon) => { */}
+
+        { 
+            this.state.amazons.filter(amazon => amazon.name.includes(this.state.searchInputValue)).map(amazon => (
+                <div key={amazon.id} className="p-4">
+                    <h3 className='font-semibold text-green-300'>{amazon.name}</h3>
+                </div>
+            ))
         }
-        </ul>
+        </div>
 
         <button 
         onClick={() => {
@@ -84,7 +131,11 @@ class Rolodex extends Component {
             Change Name
         </button>
 
-        <Button name={this.handleChange} />
+        <Button 
+            handleChange={this.props.handleChange} 
+            className="button" 
+            to='/' 
+            onClick={this.props.onClick} />
 
         </div>
         );
